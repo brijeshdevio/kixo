adf9f966296cd73cbde8e568f27944ca49adc98e
Brijesh <softbrijeshio@gmail.com>
2026-01-19 20:54:38 +0530

    feat: initialize new NestJS API application with basic health check endpoint and configuration files.

---
+++ apps/api/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 3000;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT).then(() => console.log(`Server running on port ${PORT}`)).catch(console.error);
+}
+bootstrap();
